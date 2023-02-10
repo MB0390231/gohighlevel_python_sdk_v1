c71ec3b2a1ab41fddb5c8c5dcb0bffbea0e76f3c
Michael <93745624+MB0390231@users.noreply.github.com>
2023-02-09 18:26:19 -0700

    calender appointments

--- ghlobjects/objects.py
+++ ghlobjects/objects.py
@@ -22,19 +22,46 @@ class Location(AbstractObject):
 
     def get_survey_submissions(self, params=None):
         route = "surveys/submissions"
-        token = self["apiKey"]
         subs = []
         finished_iteration = False
         page = 1
         while not finished_iteration:
             params["page"] = page
-            query = self.api.get(route=route, headers=self.api.construct_headers(token=token), params=params)
+            query = self.api.get(route=route, headers=self.api.construct_headers(token=self["apiKey"]), params=params)
             page += 1
             subs.extend(
                 [AbstractObject.create_object(data=sub, target_class=Submission) for sub in query["submissions"]]
             )
             finished_iteration = query["meta"]["nextPage"] is None
         return subs
+
+    def get_calendar_teams(self):
+        route = "calendars/teams"
+        calenders = []
+        query = self.api.get(route=route, headers=self.api.construct_headers(token=self["apiKey"]))
+        return query
+
+    def get_calendar_services(self):
+        route = "calendars/services"
+        query = self.api.get(route=route, headers=self.api.construct_headers(token=self["apiKey"]))
+        calendars = [AbstractObject.create_object(cal, target_class=Calender) for cal in query["services"]]
+        # manually inject api key
+        for cals in calendars:
+            cals["apiKey"] = self["apiKey"]
+        return calendars
+
+
+class Calender(AbstractObject):
+    def __init__(self):
+        super().__init__()
+
+    def get_appointments(self, params):
+        if not all(["startDate" in params.keys(), "endDate" in params.keys()]):
+            raise ValueError("Startdate and Enddate are required.")
+        params["calendarId"] = self["id"]
+        route = "appointments/"
+        query = self.api.get(route=route, headers=self.api.construct_headers(token=self["apiKey"]), params=params)
+        return query
 
 
 class Appointments(AbstractObject):
